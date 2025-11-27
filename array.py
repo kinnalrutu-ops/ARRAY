2319b45a293eeee2e1ca84fcf27966212ac3702b
Rutu <kinnalrutu@gmail.com>
2025-11-27 17:29:56 +0530

    Update array.py

--- array.py
+++ array.py
@@ -1,13 +1,18 @@
 import sys
 
+# Taking scores from command line
 if len(sys.argv) > 1:
     scores = list(map(int, sys.argv[1:]))
 else:
-    scores = [10, 20, 30]   
+    scores = [10, 20, 30]
 
 total = sum(scores)
 average = total / len(scores)
+maximum = max(scores)
+minimum = min(scores)
 
 print("Scores:", scores)
 print("Sum:", total)
 print("Average:", average)
+print("Maximum:", maximum)
+print("Minimum:", minimum)
